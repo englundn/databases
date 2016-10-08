cf6ce21f1b2d55c0d2a242d79367f7cee926d640
englundn <englundn@gmail.com>
2016-10-07 17:32:42 -0700

    Passing first test, implemented get and post methods to mySQL server.

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,27 @@
 var mysql = require('mysql');
 
 // Create a database connection and export it from this file.
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'chat'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.log(err, 'error');
+  } else {
+    console.log('success!');
+  }
+});
+
+// connection.end(function(err) {
+//   console.log(err, 'error');
+// });
+
+module.exports = connection;
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
